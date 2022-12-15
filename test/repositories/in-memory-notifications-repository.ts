bba92a0c6f507f6646c6f3d2453f4718386ad1ee
Felipe <pipecalgaro@gmail.com>
2022-12-15 12:52:46 -0300

    Create cancel notification use-case and tests

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -4,6 +4,28 @@ import { NotificationsRepository } from '@app/repositories/notifications-reposit
 export class InMemoryNotificationsRepository
   implements NotificationsRepository
 {
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = this.notifications.find(
+      (item) => item.id === notificationId,
+    );
+
+    if (!notification) {
+      return null;
+    }
+
+    return notification;
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const notificationIndex = this.notifications.findIndex(
+      (item) => item.id === notification.id,
+    );
+
+    if (notificationIndex >= 0) {
+      this.notifications[notificationIndex] = notification;
+    }
+  }
+
   public notifications: Notification[] = [];
 
   async create(notification: Notification) {
